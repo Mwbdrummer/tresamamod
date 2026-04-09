05a5eac62637409d5dcd705e552541182beeb91e
tresama <mattbrucell55@gmail.com>
2026-04-09 02:07:08 -0700

    Initial commit

---
+++ src/main/java/com/matt/tresamamod/TresamaModClient.java
@@ -0,0 +1,10 @@
+package com.matt.tresamamod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TresamaModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
---
+++ src/main/java/com/matt/tresamamod/TresamaModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.matt.tresamamod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TresamaModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
